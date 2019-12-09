882e3afc13c7369da7f94c6a391d6e4617517faf
Ambuj007 <ambujjnv@gmail.com>
2019-12-09 22:31:44 +0530

    adding serializers, urls, views

--- kisanhub/serializers.py
+++ kisanhub/serializers.py
@@ -2,19 +2,41 @@ from kisanhub.models import WeatherData, Metrics, Locations
 from rest_framework import serializers
 
 
-class MtericsSerializer(serializers.ModelSerializer):
+class MetricsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Metrics
-        fields = ['id', 'metrics']
+        fields = ['metric']
+
+    def create(self, validated_data):
+        print(validated_data)
 
 
 class LocationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Locations
-        fields = ['id', 'location']
+        fields = ['location']
+
+    def create(self, validated_data):
+        print(validated_data)
 
 
 class WeatherDataSerializer(serializers.ModelSerializer):
+    # metric = MetricsSerializer(write_only=True) # serializers.ModelField(MetricsSerializer, source = 'weather_data.metric', write_only=True)
+    # location = LocationSerializer(write_only=True) # serializers.ModelField(LocationSerializer, source='weather_data.location', write_only=True)
     class Meta:
         model = WeatherData
-        fields = ['value', 'year', 'month','metrics_id','location_id']
+        fields = ['value', 'year', 'month', 'metric', 'location']
+        #write_only_fields = ('metric', 'location')
+
+    def validate(self, data):
+        print(data)
+
+    def validate_location(self, value):
+        print("YAHA")
+        location = Locations.objects.get(name=value)
+        return location.id
+
+    def validate_metric(self, value):
+        print("WAHA")
+        metric = Metrics.objects.get(name=value)
+        return metric.id
--- kisanhub/views.py
+++ kisanhub/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from kisanhub.models import WeatherData, Metrics, Locations
 from rest_framework import generics
-from kisanhub.serializers import WeatherDataSerializer, MtericsSerializer, LocationSerializer
+from kisanhub.serializers import WeatherDataSerializer, MetricsSerializer, LocationSerializer
 
 # Create your views here.
 class MetricsView(generics.CreateAPIView):
     queryset = Metrics.objects.all()
-    serializer_class = MtericsSerializer
+    serializer_class = MetricsSerializer
 
 
 class LocationsView(generics.CreateAPIView):
@@ -14,8 +14,18 @@ class LocationsView(generics.CreateAPIView):
     serializer_class = LocationSerializer
 
 
-class WeatherDataView(generics.CreateAPIView):
+class WeatherDataView(generics.ListCreateAPIView):
     queryset = WeatherData.objects.all()
     serializer_class = WeatherDataSerializer
 
-
+    # def post(self, validated_data):
+    #     print("HAHA")
+    #     location = validated_data.pop('location')
+    #     print(location)
+    #     location = Location.objects.get(name=location.get('name'))
+    #     print(location)
+    #     metric = validated_data.pop('metric')
+    #     print(metric)
+    #     metric = Metric.objects.get(name=metric.get('name'))
+    #     print(metric)
+    #     WeatherData.objects.create(**validated_data, location=location, metric=metric)
